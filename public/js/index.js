08f36790a4dc04741f850ed867dea3c82dcf3b6b
Co_Zax <sushibar2929@gmail.com>
2020-08-31 03:17:08 +0200

    Added my website

---
+++ public/js/index.js
@@ -0,0 +1,45 @@
+//Import
+import colors from "./languages.js";
+
+//Functions
+//--Get github repos
+async function getRepos(username){
+    const res = await fetch(`https://api.github.com/users/${username}/repos`);
+    const data = await res.json();
+
+    data.forEach(element => {
+        const { name, html_url, fork, description, language, owner } = element;
+        const forked = fork ? `<span class="repo_detail">(<b>Forked</b>)</span>` : "";
+        const lang = language != null ? language : "";
+
+        document.getElementById("grid").innerHTML += 
+        `<div class="element">
+            <span class="repo_name"><a href="${html_url}">${name} ${forked}</a></span>
+            <span class="repo_detail">${description}</span>
+            <span class="repo_detail repo_language"><span class="repo_lang" style="background: ${colors[lang]}"></span> ${lang}</span>
+        </div>`;
+
+        //Display github avatar
+        document.getElementById("githubAvatar").src = `https://avatars3.githubusercontent.com/u/${owner.id}`;
+    });
+}
+//--Smooth scroll
+function smoothScroll(e){
+    console.log(e.dataset);
+    const offsetTop = document.querySelector(e.dataset.scrollto).offsetTop;
+    scroll({
+        top: offsetTop,
+        behavior: "smooth"
+    })
+}
+
+//Dom content loaded
+document.addEventListener("DOMContentLoaded", () => {
+    getRepos("Cozax"); //Display my repos in the page
+
+    //Smooth scrolling
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach(e => {
+        e.addEventListener("click", () => {smoothScroll(e)});
+    });
+})
